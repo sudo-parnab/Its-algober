324515e16cf354bd5a76e2c4adccaa88819d3eff
Muzhar Hussain <61931803+muzhar01@users.noreply.github.com>
2024-10-31 22:51:25 +0500

    Create MergeSort.java with implementation of the Merge Sort algorithm in Java, featuring a recursive sorting method and merge functionality

---
+++ Sorting/Merge Sort/MergeSort.java
@@ -0,0 +1,59 @@
+class MergeSort {
+    public void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[left + i];
+        for (int j = 0; j < n2; j++)
+            R[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0, k = left;
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    public void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+
+    // Example usage
+    public static void main(String[] args) {
+        MergeSort ms = new MergeSort();
+        int[] arr = {38, 27, 43, 3, 9, 82, 10};
+        ms.mergeSort(arr, 0, arr.length - 1);
+        System.out.print("Sorted array: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
